bf27f7bad386d0337049e212b6e4647cfe4acc75
aitcq <tcqai290498@gmail.com>
2023-05-18 21:00:31 +0700

    change response type

--- process/serializers.py
+++ process/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
+from task.serializers import TaskSerializer
 from .models import Process
 
 
 class ProcessSerializer(serializers.ModelSerializer):
+    tasks = serializers.SerializerMethodField()
+
     class Meta:
         model = Process
-        fields = "__all__"
+        fields = ["id", "title", "description", "tasks"]
+
+    def get_tasks(self, instance):
+        tasks = instance.subtasks.all()
+        task_serializer = TaskSerializer(tasks, many=True)
+        return task_serializer.data
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["tasks"] = self.get_tasks(instance)
+        return representation
